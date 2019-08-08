4c15e342df89b6c1d0f75d908faeaa3ca8a20040
Patrick Thibaudeau <thibaud@yorku.ca>
2019-08-08 15:23:51 -0400

    First push. Base completed

---
+++ lib.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+ 
+// We will add callbacks here as we add features to our theme.
